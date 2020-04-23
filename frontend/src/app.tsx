f2ea88dd9c2eb36f2f9e5048452bebcf3b7cee8c
Indexyz <jiduye@gmail.com>
2020-04-23 08:10:57 +0000

    feat: add eslint

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Button } from '@material-ui/core';
+import { Button } from '@material-ui/core'
 
 export default class AppComponent extends React.Component<{}, object> {
-    render() {
+    render(): React.ReactNode {
         return (
             <>
                 <h1>Hello, World</h1>
